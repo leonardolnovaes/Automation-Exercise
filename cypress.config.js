c6e20b04499bd808a2d58be632e3fb143846ff0a
Leonardo Novaes <leonardoldenovaes@gmail.com>
2025-08-20 23:49:37 -0300

    Corrigindo CI e Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 // cypress.config.js
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
@@ -10,12 +14,17 @@ module.exports = defineConfig({
     supportFile: "cypress/support/e2e.js",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor",
+      on(
+        "file:preprocessor",
         createBundler({ plugins: [createEsbuildPlugin(config)] })
       );
       return config;
     },
     baseUrl: "https://automationexercise.com",
     video: false,
+  },
+  env: {
+    allure: true,
+    allureResults: "allure-results", 
   },
 });
